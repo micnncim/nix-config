97f31d38c37600eb1d57d5198e799a80452b342d
micnncim <micnncim@gmail.com>
2022-12-11 20:18:41 +0900

    Install 1Password CLI via Homebrew
    As of writing, the CLI requires itself to be installed in /usr/local/bin/op,
    which is not a place of Nix's installation, as shown as below:
    
    > [ERROR] 2022/12/11 20:05:26 connecting to desktop app: You'll need to move
    > the CLI to /usr/local/bin/op for the 1Password app integration to work. If
    > you can't move it, you'll need to turn off Connect with 1Password CLI in the
    > 1Password app.
    
    See also https://1password.community/discussion/127950/v2-client-unable-to-connect-to-desktop-app.
    
    Signed-off-by: micnncim <micnncim@gmail.com>

--- users/micnncim/darwin.nix
+++ users/micnncim/darwin.nix
@@ -51,6 +51,16 @@ in
     enable = true;
     casks = [
       "1password"
+      # As of writing, the CLI requires itself to be installed in /usr/local/bin/op,
+      # which is not a place of Nix's installation, as shown as below:
+      #
+      # > [ERROR] 2022/12/11 20:05:26 connecting to desktop app: You'll need to move
+      # > the CLI to /usr/local/bin/op for the 1Password app integration to work. If
+      # > you can't move it, you'll need to turn off Connect with 1Password CLI in the
+      # > 1Password app.
+      #
+      # See also https://1password.community/discussion/127950/v2-client-unable-to-connect-to-desktop-app.
+      "1password-cli"
       "alt-tab"
       "bartender"
       "cron"
