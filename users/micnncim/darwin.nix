3d80a918a07b5fa7f1552dc4e0b4298129e24eb8
micnncim <micnncim@gmail.com>
2022-12-25 21:38:29 +0900

    Set `keep-outputs` and `keep-derivations` to true
    See https://github.com/nix-community/nix-direnv#via-home-manager for
    more information.
    
    Signed-off-by: micnncim <micnncim@gmail.com>

--- users/micnncim/darwin.nix
+++ users/micnncim/darwin.nix
@@ -15,6 +15,8 @@ in
     extraOptions = ''
       auto-optimise-store = true
       experimental-features = nix-command flakes
+      keep-outputs = true
+      keep-derivations = true
     '' + lib.optionalString (pkgs.system == "aarch64-darwin") ''
       extra-platforms = x86_64-darwin aarch64-darwin
     '';
