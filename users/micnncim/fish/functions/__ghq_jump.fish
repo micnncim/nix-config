4e7337cc7f5ab6e011610b1c36e26033ca129a73
micnncim <micnncim@gmail.com>
2023-02-13 15:13:21 +0900

    fish: Fix screen drawing when `cd`
    See https://github.com/fish-shell/fish-shell/issues/6413 for more
    information.
    
    Signed-off-by: micnncim <micnncim@gmail.com>

--- users/micnncim/fish/functions/__ghq_jump.fish
+++ users/micnncim/fish/functions/__ghq_jump.fish
@@ -2,6 +2,7 @@
 ghq list | fzf | read dir
 
 if [ $dir ]
-    cd (ghq root)/$dir
+    # Ref: https://github.com/fish-shell/fish-shell/issues/6413
+    cd (ghq root)/$dir; commandline -f repaint; clear
 end
 # end
