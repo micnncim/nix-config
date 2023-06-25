11b67d9852c044ad736ae30d273aa78c3b9e7ab5
micnncim <micnncim@gmail.com>
2023-06-25 16:26:34 +0900

    home-manager: Use Nord theme for fzf
    Signed-off-by: micnncim <micnncim@gmail.com>

--- users/micnncim/home-manager.nix
+++ users/micnncim/home-manager.nix
@@ -184,6 +184,22 @@ in
   programs.fzf = {
     enable = true;
     defaultOptions = [ "--reverse" "--height 40%" "--multi" "--cycle" ];
+    # Nord theme
+    # Ref: https://github.com/ianchesal/nord-fzf
+    colors = {
+      "fg" = "#e5e9f0";
+      "bg" = "#2e3440";
+      "hl" = "#81a1c1";
+      "fg+" = "#e5e9f0";
+      "bg+" = "#2e3440";
+      "hl+" = "#81a1c1";
+      "info" = "#eacb8a";
+      "prompt" = "#bf6069";
+      "pointer" = "#b48dac";
+      "marker" = "#a3be8b";
+      "spinner" = "#b48dac";
+      "header" = "#a3be8b";
+    };
   };
 
   programs.gpg = {
