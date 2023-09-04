28572772fe5fd513d2b541e3ee9675f6f4a7d26e
micnncim <micnncim@gmail.com>
2023-09-05 00:06:17 +0900

    darwin: Add substituters
    Signed-off-by: micnncim <micnncim@gmail.com>

--- users/micnncim/darwin.nix
+++ users/micnncim/darwin.nix
@@ -33,8 +33,16 @@ in
     settings = {
       trusted-users = [ "@admin" ];
 
-      substituters = [ "https://micnncim-nix-config.cachix.org" ];
-      trusted-public-keys = [ "micnncim-nix-config.cachix.org-1:VYYe2Nwe1oFIkXavwNfp9L9F5S8LCLCA5/1YaFy9lFc=" ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://micnncim-nix-config.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "micnncim-nix-config.cachix.org-1:VYYe2Nwe1oFIkXavwNfp9L9F5S8LCLCA5/1YaFy9lFc="
+      ];
     };
   };
 
