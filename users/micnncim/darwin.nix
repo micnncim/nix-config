74e175cee0552eed47e3d49a7cdfb6b32a20ea35
micnncim <micnncim@gmail.com>
2023-11-18 02:10:45 +0900

    darwin: Add Barrier
    Signed-off-by: micnncim <micnncim@gmail.com>

--- users/micnncim/darwin.nix
+++ users/micnncim/darwin.nix
@@ -77,6 +77,7 @@ in {
       # See also https://1password.community/discussion/127950/v2-client-unable-to-connect-to-desktop-app.
       "1password-cli"
       "alt-tab"
+      "barrier"
       "bartender"
       "coscreen"
       "cron"
