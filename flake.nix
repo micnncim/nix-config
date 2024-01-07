f22bd27b057a886e4e5413abebf083369c24380a
micnncim <micnncim@gmail.com>
2024-01-07 14:02:12 +0900

    flake: Update nixpkgs to 23.11
    Signed-off-by: micnncim <micnncim@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nix systems and tools by micnncim";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     flake-utils.url = "github:numtide/flake-utils";
@@ -11,7 +11,7 @@
     micnncim-nur.url = "github:micnncim/nur";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
