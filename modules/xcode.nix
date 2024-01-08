e9cdd48deba80dc4e4ea1a236ef88a485e662e1e
micnncim <micnncim@gmail.com>
2024-01-08 17:48:50 +0900

    xcode: Fix theme installation
    Signed-off-by: micnncim <micnncim@gmail.com>

--- modules/xcode.nix
+++ modules/xcode.nix
@@ -4,6 +4,7 @@ lib.mkIf pkgs.stdenv.isDarwin {
   # Seems symlink doesn't work for Xcode themes.
   # Related: https://github.com/dracula/xcode/issues/7
   home.activation.copyXcodeThemes = lib.hm.dag.entryAfter [ "linkGeneration" ] ''
+    $DRY_RUN_CMD mkdir -p ${config.home.homeDirectory}/Library/Developer/Xcode/UserData/FontAndColorThemes
     $DRY_RUN_CMD cp -f ${config.xdg.configHome}/xcode/Nord.xccolortheme ${config.home.homeDirectory}/Library/Developer/Xcode/UserData/FontAndColorThemes/Nord.xccolortheme
   '';
 
