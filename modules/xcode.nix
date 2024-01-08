30e158fd93afae7ca3ef52302f5f57f35e58d378
micnncim <micnncim@gmail.com>
2024-01-08 17:13:18 +0900

    xcode: Fix theme copy
    Signed-off-by: micnncim <micnncim@gmail.com>

--- modules/xcode.nix
+++ modules/xcode.nix
@@ -1,10 +1,16 @@
 { config, lib, pkgs, ... }:
 
 lib.mkIf pkgs.stdenv.isDarwin {
+  xdg.configFile."xcode" = {
+    recursive = true;
+    source = ./xcode;
+  };
+
   # Seems symlink doesn't work for Xcode themes.
   # Related: https://github.com/dracula/xcode/issues/7
-  # home.file."${config.home.homeDirectory}/Library/Developer/Xcode/UserData/FontAndColorThemes/Nord.xccolortheme".source =
-  #   ./xcode/Nord.xccolortheme;
+  home.activation.copyXcodeThemes = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    $DRY_RUN_CMD cp -f ${config.xdg.configHome}/xcode/Nord.xccolortheme ${config.home.homeDirectory}/Library/Developer/Xcode/UserData/FontAndColorThemes/Nord.xccolortheme
+  '';
 
   home.file."${config.home.homeDirectory}/Library/Developer/Xcode/UserData/IDETemplateMacros.plist".source =
     ./xcode/IDETemplateMacros.plist;
