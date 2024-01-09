9cd867fe7fd1c897580be56b2ced7de2f142322d
micnncim <micnncim@gmail.com>
2024-01-09 14:31:52 +0900

    darwin: Refactor system
    Signed-off-by: micnncim <micnncim@gmail.com>

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -1,37 +1,39 @@
 { config, lib, pkgs, username, ... }:
 
 {
-  # Currently, application aren't linked to /Application.
-  # https://github.com/LnL7/nix-darwin/issues/139#issuecomment-663117229
-  system.build.applications = pkgs.lib.mkForce (pkgs.buildEnv {
-    name = "applications";
-    paths = config.environment.systemPackages
-      ++ config.home-manager.users.${username}.home.packages;
-    pathsToLink = "/Applications";
-  });
+  system = {
+    # Currently, application aren't linked to /Application.
+    # https://github.com/LnL7/nix-darwin/issues/139#issuecomment-663117229
+    build.applications = pkgs.lib.mkForce (pkgs.buildEnv {
+      name = "applications";
+      paths = config.environment.systemPackages
+        ++ config.home-manager.users.${username}.home.packages;
+      pathsToLink = "/Applications";
+    });
 
-  system.keyboard = {
-    enableKeyMapping = true;
-    remapCapsLockToControl = true;
-  };
-
-  # TODO(micnncim): Add more.
-  system.defaults = {
-    NSGlobalDomain = {
-      "com.apple.trackpad.scaling" = 3.0;
-      InitialKeyRepeat = 20;
-      KeyRepeat = 1;
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToControl = true;
     };
 
-    dock.autohide = true;
+    # TODO(micnncim): Add more.
+    defaults = {
+      NSGlobalDomain = {
+        "com.apple.trackpad.scaling" = 3.0;
+        InitialKeyRepeat = 20;
+        KeyRepeat = 1;
+      };
 
-    screencapture.location =
-      "/Users/${username}/Google Drive/My Drive/Screenshots";
+      dock.autohide = true;
 
-    trackpad.Clicking = true;
+      screencapture.location =
+        "/Users/${username}/Google Drive/My Drive/Screenshots";
+
+      trackpad.Clicking = true;
+    };
+
+    activationScripts.vscode.text = ''
+      defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+    '';
   };
-
-  system.activationScripts.vscode.text = ''
-    defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-  '';
 }
