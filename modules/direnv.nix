3d43e61f860791c07533c1d8fd8838b6963f0ea9
micnncim <micnncim@gmail.com>
2024-01-15 22:04:30 +0900

    direnv: Add whitelist
    Signed-off-by: micnncim <micnncim@gmail.com>

--- modules/direnv.nix
+++ modules/direnv.nix
@@ -1,8 +1,20 @@
 { config, lib, pkgs, ... }:
 
+let
+  homeDir = config.home.homeDirectory;
+in
 {
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    config = {
+      whitelist = {
+        prefix = [
+          "${homeDir}/src/github.com/micnncim"
+          "${homeDir}/src/github.com/ergastirio"
+          "${homeDir}/src/github.com/kouzoh"
+        ];
+      };
+    };
   };
 }
