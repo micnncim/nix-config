5bbf41ab04d58fd56e0f76f8bd23f5baf231ad99
micnncim <micnncim@gmail.com>
2024-09-01 18:11:30 +0900

    shell: Add env var and alias for 1Password CLI
    Signed-off-by: micnncim <micnncim@gmail.com>

--- modules/shell/default.nix
+++ modules/shell/default.nix
@@ -4,22 +4,23 @@
   xdg.enable = true;
 
   home.sessionVariables = {
-    LANG = "en_US.UTF-8";
-    LC_CTYPE = "UTF-8";
-    LC_ALL = "en_US.UTF-8";
     EDITOR = "nvim";
-    PAGER = "less";
+    LANG = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+    LC_CTYPE = "UTF-8";
     MANPAGER = "sh -c 'col -bx | ${pkgs.bat}/bin/bat -l man -p'";
     # https://github.com/sharkdp/bat/issues/652#issuecomment-529032263
     MANROFFOPT = "-c";
+    OP_PLUGIN_ALIASES_SOURCED = "1";
+    PAGER = "less";
     # https://cloud.google.com/blog/products/containers-kubernetes/kubectl-auth-changes-in-gke
     USE_GKE_GCLOUD_AUTH_PLUGIN = "True";
   };
 
   home.shellAliases = {
+    gh = "op plugin run -- gh";
     ll = "eza -la";
-    llt =
-      "eza -laTF --git --group-directories-first --git-ignore --ignore-glob .git";
+    llt = "eza -laTF --git --group-directories-first --git-ignore --ignore-glob .git";
     ls = "eza";
     lt = "eza -T";
   };
