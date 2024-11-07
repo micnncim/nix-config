55cf2ca2a89cac76ec8250322d83b008b4c43464
micnncim <micnncim@gmail.com>
2024-11-07 13:10:37 +0900

    machines: Clean up configurations
    Signed-off-by: micnncim <micnncim@gmail.com>

--- machines/altair/default.nix
+++ machines/altair/default.nix
@@ -1,18 +1,3 @@
 { config, lib, pkgs, ... }:
 
-{
-  system.defaults = {
-    # All the following numbers of concurrent tasks are the same as
-    # `sysctl -n hw.ncpu`.
-    CustomUserPreferences = {
-      "com.apple.dt.Xcode" = {
-        "IDEBuildOperationMaxNumberOfConcurrentCompileTasks" = 10;
-        "PBXNumberOfParallelBuildSubtasks" = 10;
-      };
-      "com.apple.dt.xcodebuild" = {
-        "IDEBuildOperationMaxNumberOfConcurrentCompileTasks" = 10;
-        "PBXNumberOfParallelBuildSubtasks" = 10;
-      };
-    };
-  };
-}
+{ }
