b53513e89dbf7046afa4bfe53c77056a769617ce
micnncim <micnncim@gmail.com>
2024-12-02 18:42:52 +0900

    tmux: Tweak appearance
    Signed-off-by: micnncim <micnncim@gmail.com>

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -26,14 +26,14 @@
       extraConfig = ''
         set -g @catppuccin_window_status_style 'rounded'
         set -g @catppuccin_window_default_fill "number"
-        set -g @catppuccin_window_default_text "#W"
+        set -g @catppuccin_window_text " #W"
+        set -g @catppuccin_window_current_text " #W"
         set -g @catppuccin_window_current_fill "number"
         set -g @catppuccin_status_modules_right "date_time"
         set -g @catppuccin_status_modules_left "session"
         set -g @catppuccin_status_right_separator_inverse "no"
         set -g @catppuccin_status_fill "icon"
         set -g @catppuccin_status_connect_separator "no"
-        set -g @catppuccin_directory_text "#{b:pane_current_path}"
         set -g @catppuccin_date_time_text "%H:%M"
       '';
     };
