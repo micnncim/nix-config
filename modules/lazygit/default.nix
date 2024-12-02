6bd43ec78934f427283f01b71acc0bbbaedc766a
micnncim <micnncim@gmail.com>
2024-12-02 20:11:55 +0900

    lazygit: Update config
    Signed-off-by: micnncim <micnncim@gmail.com>

--- modules/lazygit/default.nix
+++ modules/lazygit/default.nix
@@ -13,11 +13,21 @@
         language = "en";
         commitAuthorShortLength = 16;
         commitAuthorLongLength = 24;
+        spinner.frames = [ "⣾" "⣽" "⣻" "⢿" "⡿" "⣟" "⣯" "⣷" ];
       };
       update.method = "never";
       quitOnTopLevelReturn = true;
       disableStartupPopups = true;
       promptToReturnFromSubprocess = false;
+      customCommands = [
+        {
+          key = "O";
+          context = "global";
+          command = "git push && op plugin run -- gh pr create --fill --web";
+          description = "Create pull request";
+          loadingText = "Creating pull request...";
+        }
+      ];
     };
   };
 }
