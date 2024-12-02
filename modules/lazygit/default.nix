35b279a8466951445241d84fb703a36a13badce1
micnncim <micnncim@gmail.com>
2024-12-02 17:41:44 +0900

    lazygit: Configure more options
    Signed-off-by: micnncim <micnncim@gmail.com>

--- modules/lazygit/default.nix
+++ modules/lazygit/default.nix
@@ -5,8 +5,19 @@
     enable = true;
     settings = {
       git = {
+        commit.signOff = true;
         paging.pager = "${pkgs.delta}/bin/delta --dark --paging=never";
+        disableForcePushing = true;
       };
+      gui = {
+        language = "en";
+        commitAuthorShortLength = 16;
+        commitAuthorLongLength = 24;
+      };
+      update.method = "never";
+      quitOnTopLevelReturn = true;
+      disableStartupPopups = true;
+      promptToReturnFromSubprocess = false;
     };
   };
 }
