d19875a58ed185861952f41b9f84a70337c95d82
micnncim <micnncim@gmail.com>
2024-12-03 23:11:53 +0900

    machines/altair: Add lazygit branch prefix
    Signed-off-by: micnncim <micnncim@gmail.com>

--- machines/altair/default.nix
+++ machines/altair/default.nix
@@ -1,6 +1,16 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, username, ... }:
 
 {
   ids.uids.nixbld = 300;
   ids.gids.nixbld = 30000;
+
+  home-manager.users.${username} = { pkgs, ... }: {
+    programs.lazygit = {
+      settings = {
+        git = {
+          branchPrefix = "micnncim/";
+        };
+      };
+    };
+  };
 }
