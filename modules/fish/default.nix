1cb38289ac61a2ea24ecf298e77fc81a4f18a3d5
micnncim <micnncim@gmail.com>
2024-12-08 13:23:56 +0900

    fish: Refactor
    Signed-off-by: micnncim <micnncim@gmail.com>

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -2,6 +2,10 @@
 
 let
   sources = import ../../nix/sources.nix;
+
+  functions = builtins.filter
+    (lib.hasSuffix ".fish")
+    (lib.filesystem.listFilesRecursive ./functions);
 in
 {
   programs.fish = {
@@ -18,18 +22,45 @@ in
         name = (lib.strings.removeSuffix ".fish" (builtins.baseNameOf path));
         value = (builtins.readFile path);
       })
-      (builtins.filter
-        (lib.hasSuffix ".fish")
-        (lib.filesystem.listFilesRecursive ./functions)
+      functions
+    );
+    shellAbbrs = {
+      bat = { expansion = "bat"; regex = "^cat$"; };
+      cursor = { expansion = "cursor"; regex = "^c$"; };
+      ghq = { expansion = "ghq get -p"; regex = "^ghq$"; };
+      git = { expansion = "git"; regex = "^g$"; };
+      help = { expansion = "help"; regex = "^h$"; };
+      kubectl = { expansion = "kubectl"; regex = "^k$"; };
+      kubectx = { expansion = "kubectx"; regex = "^kc$"; };
+      kubens = { expansion = "kubens"; regex = "^kn$"; };
+      make = { expansion = "make"; regex = "^m$"; };
+      mkdir = { expansion = "mkdir -p"; regex = "^mkdir$"; };
+      nvim = { expansion = "nvim"; regex = "^v$"; };
+      rip = { expansion = "rip"; regex = "^rm$"; };
+      xcode = { expansion = "xcode"; regex = "^x$"; };
+    } // (
+      builtins.listToAttrs (map
+        (path:
+          let
+            basename = lib.strings.removeSuffix ".fish" (builtins.baseNameOf path);
+            parts = lib.strings.splitString "_" (lib.strings.removePrefix "__abbr_" basename);
+            type = builtins.elemAt parts 0; # "flag" or "subcommand"
+            name = builtins.elemAt parts 1;
+          in
+          {
+            name = if type == "flag" then "-${name}" else "${name}";
+            value = {
+              function = basename;
+              position = "anywhere";
+            };
+          })
+        (
+          builtins.filter
+            (path: lib.hasPrefix "__abbr_" (builtins.baseNameOf path))
+            functions
+        )
       )
     );
-    interactiveShellInit = lib.strings.concatStrings
-      (lib.strings.intersperse "\n" [
-        "fish_add_path /etc/profiles/per-user/${username}/bin"
-        "source ${pkgs.google-cloud-sdk}/google-cloud-sdk/path.fish.inc"
-        "set -x LS_COLORS \"$(${pkgs.vivid}/bin/vivid generate catppuccin-mocha)\""
-        (builtins.readFile ./config.fish)
-      ]);
     plugins = map
       (n: {
         name = n;
@@ -40,5 +71,21 @@ in
       "fish-gcloud-completion"
       "fish-nix-env"
     ];
+    interactiveShellInit = ''
+      fish_add_path /opt/homebrew/bin
+      fish_add_path $GOBIN
+
+      source ${pkgs.google-cloud-sdk}/google-cloud-sdk/path.fish.inc
+
+      set -x LS_COLORS \"$(${pkgs.vivid}/bin/vivid generate catppuccin-mocha)\"
+
+      if isatty
+        set -x GPG_TTY (tty)
+      end
+
+      if not set -q TMUX
+        exec tmux; or echo "failed to launch tmux"
+      end
+    '';
   };
 }
