387b02d74288812ce39f4c184f6540913fa9fc9c
micnncim <micnncim@gmail.com>
2024-12-23 01:15:38 +0900

    flake: Update deps
    Signed-off-by: micnncim <micnncim@gmail.com>

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -32,22 +32,6 @@
         '';
       }
     ];
-    catppuccin = {
-      extraConfig = ''
-        set -g @catppuccin_window_status_style 'rounded'
-        set -g @catppuccin_window_default_fill 'number'
-        set -g @catppuccin_window_text ' #W'
-        set -g @catppuccin_window_current_text ' #W'
-        set -g @catppuccin_window_current_fill 'number'
-        set -g @catppuccin_status_modules_right 'date_time'
-        set -g @catppuccin_status_modules_left 'session'
-        set -g @catppuccin_status_right_separator_inverse 'no'
-        set -g @catppuccin_status_fill 'icon'
-        set -g @catppuccin_status_connect_separator 'no'
-        set -g @catppuccin_date_time_text '%H:%M'
-        set -g @catppuccin_application_icon ' '
-      '';
-    };
     extraConfig = builtins.readFile ./tmux.conf;
   };
 }
