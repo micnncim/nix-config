9dee9565cfd944ecae601c81c88e24a15bfa6dba
micnncim <micnncim@gmail.com>
2024-12-31 15:05:19 +0900

    tmux: Remove unused plugins
    Signed-off-by: micnncim <micnncim@gmail.com>

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -19,18 +19,8 @@
       logging
       open
       prefix-highlight
-      resurrect
       tmux-fzf
       yank
-      {
-        plugin = continuum;
-        extraConfig = ''
-          set -g @continuum-restore 'on'
-          set -g @continuum-save-interval '5' # minutes
-          set -g @continuum-boot 'on'
-          set -g @continuum-boot-options 'alacritty'
-        '';
-      }
     ];
     extraConfig = builtins.readFile ./tmux.conf;
   };
